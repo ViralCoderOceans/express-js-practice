a363eff1fe4c71b22553098dcfa525a9a04ab8e8
Viral Nakrani <viral.coderoceans@gmail.com>
2023-09-13 17:28:03 +0530

    update graphql

--- app.js
+++ app.js
@@ -30,31 +30,31 @@ app.use('/files', filesRouter);
 app.use('/mongoDB', mongoDBRouter);
 app.use('/graphql', graphqlRouter);
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
-// Replace the placeholder with your Atlas connection string
-const uri = "mongodb+srv://viralnakrani:viralnakrani@thevknakrani.dvqlomp.mongodb.net/";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri,  {
-        serverApi: {
-            version: ServerApiVersion.v1,
-            strict: true,
-            deprecationErrors: true,
-        }
-    }
-);
-async function run() {
-  try {
-    // Connect the client to the server (optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+// const { MongoClient, ServerApiVersion } = require("mongodb");
+// // Replace the placeholder with your Atlas connection string
+// const uri = "mongodb+srv://viralnakrani:viralnakrani@thevknakrani.dvqlomp.mongodb.net/";
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri,  {
+//         serverApi: {
+//             version: ServerApiVersion.v1,
+//             strict: true,
+//             deprecationErrors: true,
+//         }
+//     }
+// );
+// async function run() {
+//   try {
+//     // Connect the client to the server (optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
